1ac2d3361040c7a1cb766a1fac7feb3707d82f95
dylanirlbeck <dylanirlbeck@gmail.com>
2021-08-09 18:58:22 -0500

    Fix workflow syntax

--- .github/scripts/sendMessage.js
+++ .github/scripts/sendMessage.js
@@ -1,4 +1,47 @@
-// print process.argv
-process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
+// Require the Node Slack SDK package (github.com/slackapi/node-slack-sdk)
+const { WebClient, LogLevel } = require("@slack/web-api");
+const fs = require("fs");
+
+// WebClient insantiates a client that can call API methods
+// When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+const client = new WebClient(process.env.API_KEY, {
+  //   // LogLevel can be imported and used to make debugging simpler
+  logLevel: LogLevel.DEBUG,
 });
+
+// Post a message to a channel your app is in using ID and message text
+async function publishMessage(text) {
+  try {
+    // Call the chat.postMessage method using the built-in WebClient
+    const result = await client.chat.postMessage({
+      // The token you used to initialize your app
+      token: process.env.API_KEY,
+      channel: "C02AJN43BD4",
+      text: text,
+      // You could also use a blocks[] array to send richer content
+    });
+
+    // Print result, which includes information about the message (like TS)
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function formatMessage(newEmployee) {
+  const { name, preferredFirstName, pronouns, city, favoriteMovie, funFact } =
+    newEmployee;
+  return `Let's all welcome *${name}* (${pronouns}, ${city}) to TTS!\n\nLet's learn more about ${preferredFirstName}:\n- *Favorite movie*: ${favoriteMovie}\n- *Fun fact*: ${funFact}`;
+}
+
+if (process.argv.length > 2) {
+  const addedFiles = process.argv.slice(2);
+
+  addedFiles.forEach((addedFile) => {
+    let rawdata = fs.readFileSync(addedFile);
+    let newEmployee = JSON.parse(rawdata);
+
+    console.log("Added file", addedFile);
+    publishMessage(formatMessage(newEmployee));
+  });
+}
--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # hello github
 
-TODO this README
+This is an instruction repository designed to help new TTS employees onboard to
+GitHub. It encourages learning by doing: every new employee is supposed to make
+a pull request in this repository and see a welcome message posted to the
+`#welcomes` slack channel!
+
+## Who
+
+New employees at TTS who don't feel comfortable in GitHub.
+
+## Why
+
+GitHub is an essential tool to our work at TTS. We use it to host nearly all of
+our code, most of which is open-source. However, comfort levels with GitHub vary
+widely across TTS. And especially given that the [TTS RFC
+process](https://github.com/18f/tts-rfc) is now
+conducted mostly on GitHub, it is imperative that all TTS staff feel comfortable
+with this tool.
+
+## What
+
+We have a number of goals with this approach to learning GitHub:
+
+- Goal 1
+- Another goal
+- A third goal
+
+## How
